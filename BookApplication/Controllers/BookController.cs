c8b69fc79e52b83a0bb835283bae8c5c4c31f343
İrem Ceylan <iremceylan0634@hotmail.com>
2024-02-27 12:33:50 +0300

    sd
    Book Application

---
+++ BookApplication/Controllers/BookController.cs
@@ -0,0 +1,54 @@
+﻿using BookApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System; // Add this line for DateTime
+using System.Collections.Generic; // Add this line for List<T>
+
+namespace BookApplication.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book/DisplayBooks
+        public IActionResult DisplayBooks()
+        {
+            
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    Name = "İnce Memed",
+                    Author = "Yaşar Kemal",
+                    PageNumber = 200,
+                    DueDate = DateTime.Now.AddDays(30),
+                    IsHomeworkUploaded = true
+                },
+                new Book
+                {
+                    Name = "Ateşten Gömlek",
+                    Author = "Halide Edip Adıvar",
+                    PageNumber = 250,
+                    DueDate = DateTime.Now.AddDays(10),
+                    IsHomeworkUploaded = false
+                },
+
+                new Book
+                {
+                    Name = "Example Book",
+                    Author = "John Doe",
+                    PageNumber = 150,
+                    DueDate = DateTime.Now.AddDays(5), // Due in 5 days
+                    IsHomeworkUploaded = false
+                 },
+                new Book
+                {
+                    Name = "Yüzyıllık Yalnızlık",
+                    Author = "Gabriel Garcia Marquez",
+                    PageNumber = 300,
+                    DueDate = DateTime.Now.AddDays(3),
+                    IsHomeworkUploaded = true
+                }
+            };
+
+            return View("DisplayBooks", books);
+        }
+    }
+}
---
+++ BookApplication/Models/Book.cs
@@ -0,0 +1,35 @@
+﻿namespace BookApplication.Models
+{
+    public class Book
+    {
+        public string Name { get; set; } = "";
+        public string Author { get; set; } = "";
+        public int PageNumber { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsHomeworkUploaded { get; set; }
+
+        public string GetDueDateColor()
+        {
+            if (IsHomeworkUploaded)
+            {
+                return "green"; // Homework is uploaded on time
+            }
+
+            TimeSpan timeRemaining = DueDate - DateTime.Now;
+            int daysRemaining = (int)timeRemaining.TotalDays;
+
+            if (daysRemaining < 0)
+            {
+                return "red"; // Due date has passed and homework is not uploaded
+            }
+            else if (daysRemaining <= 7)
+            {
+                return "yellow"; // Less than or equal to 7 days remaining and homework is not uploaded
+            }
+            else
+            {
+                return "red"; // More than 7 days remaining and homework is not uploaded
+            }
+        }
+    }
+}
